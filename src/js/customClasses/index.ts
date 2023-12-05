23e801d034c0193e74f711f516e7ca745f22b730
Patrick Lake <patricklake@PatrickLakesMac.attlocal.net>
2023-12-05 02:45:27 -0500

    Add classes to csutom class list to avoid purging them

--- src/js/customClasses/index.ts
+++ src/js/customClasses/index.ts
@@ -3,6 +3,11 @@ const customClasses = [
   "fade-out-backward",
   "fade-out-forward",
   "fade-in-backward",
+  "image-gallery__main-image-overlay",
+  "image-gallery__main-image-overlay-content",
+  "title",
+  "description",
+  "image-gallery__thumbnail",
 ]
 
 export default customClasses
